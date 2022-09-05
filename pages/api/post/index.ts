d9a9cd424d1edb8e6d4fa798ebd2277446139ee7
SorinC6 <kish.sorin@yahoo.com>
2022-09-05 17:58:08 +0300

    add main feed videos and queries

---
+++ pages/api/post/index.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { allPostsQuery } from "../../../utils/queries";
+import { client } from "../../../utils/client";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "GET") {
+    const query = allPostsQuery();
+    const data = await client.fetch(query);
+    res.status(200).json(data);
+  }
+}
