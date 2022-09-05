472f4ccc67c876845d93052b5c5fbf206f36ab9b
SorinC6 <kish.sorin@yahoo.com>
2022-09-05 13:01:04 +0300

    add navbar basic styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -8,12 +10,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
     >
       <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
-        {/* <Navbar /> */}
-        Navbar
+        <Navbar />
         <div className="flex gap-6 md:gap-20 ">
           <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-            {/* <Sidebar /> */}
-            Sidebar
+            <Sidebar />
           </div>
           <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
             <Component {...pageProps} />
