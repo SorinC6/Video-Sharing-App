2e8c05389bda1e72dd1d297b4a08cfb23169190e
SorinC6 <kish.sorin@yahoo.com>
2022-09-07 11:04:42 +0300

    add upload video functionality

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -2,17 +2,19 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { allPostsQuery } from "../../../utils/queries";
 import { client } from "../../../utils/client";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   if (req.method === "GET") {
     const query = allPostsQuery();
     const data = await client.fetch(query);
     res.status(200).json(data);
+  } else if (req.method === "POST") {
+    const doc = req.body;
+
+    client.create(doc).then(() => {
+      res.status(201).json("video created");
+    });
   }
 }
